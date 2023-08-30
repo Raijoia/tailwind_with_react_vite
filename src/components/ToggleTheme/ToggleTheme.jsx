358c1d94a21db0562a08289c3e23d64b241d412c
Raí Joia <raijoiamv@gmail.com>
2023-08-30 10:35:38 -0300

    aplication toggleMode in theme system

--- src/components/ToggleTheme/ToggleTheme.jsx
+++ src/components/ToggleTheme/ToggleTheme.jsx
@@ -1,9 +1,18 @@
 import { MoonIcon, SunIcon } from '@heroicons/react/24/solid'
+import { useEffect } from 'react'
 
 export default function ToggleTheme() {
+  const systemPreference = window.matchMedia('(prefers-color-scheme: dark)').matches
+  const pageClasses = document.documentElement.classList
+
+  // quando o systempreference retornar true (theme dark no sistema do usuário) adiciona o theme dark na página 
+  useEffect(() => {
+    systemPreference && pageClasses.add("dark")
+  })
+
   // quando clicar, colocar o class dark no root da página 
   const toggle = () => {
-    document.documentElement.classList.toggle('dark')
+    pageClasses.toggle('dark')
   }
 
   return (
