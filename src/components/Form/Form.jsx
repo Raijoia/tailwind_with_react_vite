02cc02420d888c14b0b46c0a24ca8999a6eff1a6
Raí Joia <raijoiamv@gmail.com>
2023-08-30 13:15:41 -0300

    style button with hover

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -12,7 +12,7 @@ export default function Form({ onSubmit }) {
     <form onSubmit={safeSubmit} className="h-full flex flex-col items-center justify-center -mt-20 mx-5 gap-10">
       <Input required type="text" placeholder="Insira seu nome..."  />
       <Input required type="email" placeholder="Insira seu email..." />
-      <button type="submit" className="py-1 px-5 w-full max-w-sm bg-alura-100 dark:bg-dark-200 rounded-full text-gray-200 uppercase outline-none">Seguir</button>
+      <button type="submit" className="py-1 px-5 w-full max-w-sm bg-alura-100 dark:bg-dark-200 rounded-full text-gray-200 uppercase outline-none hover:animate-pulse hover:shadow-md hover:shadow-gray-500 hover:dark:shadow-black">Seguir</button>
     </form>
   )
 }
